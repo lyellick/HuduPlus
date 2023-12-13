bafd95b6a022ae531d6e39247c12229cab3a1182
Lincoln Yellick <lyellick@bluenetinc.com>
2023-12-13 13:43:59 -0600

    .

--- src/content.js
+++ src/content.js
@@ -2,68 +2,98 @@ var key = "";
 
 const readLocalStorage = async (key) => {
     return new Promise((resolve, reject) => {
-      chrome.storage.local.get([key], function (result) {
-        if (result[key] === undefined) {
-          reject();
-        } else {
-          resolve(result[key]);
-        }
-      });
+        chrome.storage.local.get([key], function (result) {
+            if (result[key] === undefined) {
+                reject();
+            } else {
+                resolve(result[key]);
+            }
+        });
     });
-  };
+};
 
 (async function () {
     // await chrome.storage.local.remove('hudu');
-    key = await readLocalStorage('hudu');
+    try {
+        key = await readLocalStorage('hudu');
+    } catch {
+        await chrome.storage.local.remove('hudu');
+    }
 
     if (key === "") {
         key = prompt("Hudu API Key", "");
-
-        await chrome.storage.local.set({ hudu: key });
+        if (key !== "") {
+            await chrome.storage.local.set({ hudu: key });
+        }
     }
 
     const params = new URLSearchParams(window.location.search);
     const companyIdParam = params.get('company_id');
     const folderParam = params.get('folder');
 
-    if (companyIdParam && folderParam === null && location.pathname === "/kba") {
+    if (companyIdParam && folderParam === null && key !== "" && location.pathname === "/kba") {
         await showTree(companyIdParam);
+    } else {
+        console.log("Hudu+: Unable to load API Key.");
+        await chrome.storage.local.remove('hudu');
     }
 })();
 
 async function showTree(companyIdParam) {
+
     const companyId = parseInt(companyIdParam);
 
-    const articles = await fetchApiData(`/api/v1/articles?company_id=${companyId}`);
-    const folders = await fetchApiData(`/api/v1/folders?company_id=${companyId}`);
-
-    const tree = buildTree(folders, articles);
-
-    document.getElementsByClassName("index__folders")[0].innerHTML = "";
-
-    tree.forEach(rootNode => {
-        walkData(rootNode);
+    await fetchApiData(`/api/v1/articles?company_id=${companyId}`).then(data => {
+        articles = data;
     });
+    
+    await fetchApiData(`/api/v1/folders?company_id=${companyId}`).then(data => {
+        folders = data;
+    });
+
+    const tree = await buildTree(folders, articles);
+
+    if (tree !== undefined) {
+        document.getElementsByClassName("index__folders")[0].innerHTML = "";
+
+        tree.forEach(rootNode => {
+            walkData(rootNode);
+        });
+    } else {
+        console.log("Hudu+: Bad API Key.");
+        await chrome.storage.local.remove('hudu');
+    }
 }
 
-async function fetchApiData(url) {
-    const response = await fetch(url, { headers: { "x-api-key": key } });
-    const data = await response.json();
-    return data.articles || data.folders || [];
+function fetchApiData(url) {
+    return fetch(url, { headers: { "x-api-key": key } })
+        .then(response => response.json())
+        .then(data => data.articles || data.folders || [])
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            return []; // Return an empty array or handle the error as needed
+        });
 }
 
-function buildTree(data, articles, parentId = null) {
-    const folders = data.filter(item => item.parent_folder_id === parentId);
+async function buildTree(data, articles, parentId = null) {
+    if (data.length > 0) {
+        const folders = data.filter(item => item.parent_folder_id === parentId);
 
-    return folders.map(folder => {
-        const children = buildTree(data, articles, folder.id);
-        const found = articles.filter(item => item.folder_id === folder.id);
+        const folderPromises = folders.map(async folder => {
+            const children = await buildTree(data, articles, folder.id);
+            const found = articles.filter(item => item.folder_id === folder.id);
 
-        folder.children = children || [];
-        folder.articles = found || [];
+            folder.children = children || [];
+            folder.articles = found || [];
 
-        return folder;
-    });
+            return folder;
+        });
+
+        return Promise.all(folderPromises);
+    }
+
+    // Return a resolved promise with an empty array if there are no folders
+    return Promise.resolve([]);
 }
 
 function walkData(node, level = 0) {
