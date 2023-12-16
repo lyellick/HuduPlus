c0e7bc60df3e2eaec51b1096fb1bb6ecb996ce42
Lincoln Hach Yellick <31251168+lyellick@users.noreply.github.com>
2023-12-15 22:04:22 -0600

    .

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -42,18 +42,25 @@
         clearTimeout(typingTimer);
         if (searchCompanyInput.value) {
             typingTimer = setTimeout(search, doneTypingInterval);
+        } else {
+            document.getElementById('company-select').innerHTML = `<option style="display: none">Select Company</option>`;
+            updateInputs();
         }
     });
-    
+
     async function search() {
-        var search = await searchCompanies(instance, document.getElementById("company-name").value, key);
-        document.getElementById('company-select').innerHTML = `<option selected disabled="disabled">Select Company</option>`;
+        document.getElementById('password-save').classList.remove('d-none');
+        clearTimeout(save);
+        var value = document.getElementById("company-name").value;
+        var search = await searchCompanies(instance, value, key);
+        document.getElementById('company-select').innerHTML = `<option style="display: none">Select Company</option>`;
         search.companies.forEach(element => {
             var opt = document.createElement('option');
             opt.value = element.id;
             opt.innerHTML = element.name;
             document.getElementById('company-select').appendChild(opt);
         });
+        save = setTimeout(function () { document.getElementById('password-save').classList.add('d-none'); }, 1000);
     }
 
     document.getElementById('company-select').addEventListener('change', function () {
@@ -61,13 +68,8 @@
     });
 
     document.getElementById('password-wrapper').addEventListener('input', () => {
-        checkInputs('password-wrapper', 'psw-naming-convention-apply', [ "username" ]) 
-        var company = document.getElementById('company-select').options.length !== 1 ? document.getElementById('company-select').options[document.getElementById('company-select').selectedIndex].text : "{CLIENT}";
-        var location = document.getElementById('location').value !== "" ? document.getElementById('location').value : "[ \"Global\" | {LOCATION} ]";
-        var service = document.getElementById('service').value !== "" ? document.getElementById('service').value : "{SERVICE}";
-        var account = document.getElementById('account').value !== "" ? document.getElementById('account').value : "{ACCOUNT}";
-        
-        document.getElementById("password-title-preview").innerHTML = `${company} - ${location} - ${service} - ${account}`;
+        checkInputs('password-wrapper', 'psw-naming-convention-apply', ["username", "password-title-preview"])
+        updateInputs();
     });
 
     document.getElementById('psw-naming-convention').addEventListener('hide.bs.modal', () => {
@@ -78,13 +80,15 @@
         var body = {
             asset_password: {
                 password: document.getElementById('password').value,
-                name: document.getElementById("password-title-preview").innerHTML,
+                name: document.getElementById("password-title-preview").getAttribute("placeholder"),
                 company_id: parseInt(document.getElementById("password-title-preview").getAttribute('company-id')),
                 username: document.getElementById('username').value
             }
         };
 
         if (confirm(`Are you sure you want to create \"${body.asset_password.name}\" for ${document.getElementById('company-select').options[document.getElementById('company-select').selectedIndex].text}?`)) {
+            document.getElementById('password-save').classList.remove('d-none');
+            clearTimeout(save);
             var headers = new Headers();
             headers.append("x-api-key", key);
             headers.append("Content-Type", "application/json");
@@ -97,21 +101,32 @@
                 return r.json();
             }).then(data => {
                 console.log(data);
-                document.getElementById('password-alert').innerHTML = `<div class="alert alert-success alert-dismissible fade show px-3 py-2 small mt-2" role="alert">
-                Added password: <a href="${data.asset_password.url}" target="_blank">${data.asset_password.name}</a>
+                document.getElementById('password-alert').innerHTML = `<div class="alert alert-success alert-dismissible fade show px-3 py-2 tiny mt-2" role="alert">
+                Added: <a href="${data.asset_password.url}" target="_blank">${data.asset_password.name}</a>
                 <button type="button" class="btn-close small" data-bs-dismiss="alert" aria-label="Close" style="padding:.8rem .8rem"></button>
               </div>`
                 clearPasswordInputs()
+                save = setTimeout(function () { document.getElementById('password-save').classList.add('d-none'); }, 1000);
             }).catch((error) => {
-                document.getElementById('password-alert').innerHTML = `<div class="alert alert-danger alert-dismissible fade show px-3 py-2 small mt-2" role="alert">
+                document.getElementById('password-alert').innerHTML = `<div class="alert alert-danger alert-dismissible fade show px-3 py-2 tiny mt-2" role="alert">
                 Failed to add password.
                 <button type="button" class="btn-close small" data-bs-dismiss="alert" aria-label="Close" style="padding:.8rem .8rem"></button>
               </div>`
                 console.error("Error:", error);
+                save = setTimeout(function () { document.getElementById('password-save').classList.add('d-none'); }, 1000);
             });
         }
     });
 })();
+
+function updateInputs() {
+    var company = document.getElementById('company-select').options.length !== 1 ? document.getElementById('company-select').options[document.getElementById('company-select').selectedIndex].text : "{CLIENT}";
+    var location = document.getElementById('location').value !== "" ? document.getElementById('location').value : "[ \"Global\" | {LOCATION} ]";
+    var service = document.getElementById('service').value !== "" ? document.getElementById('service').value : "{SERVICE}";
+    var account = document.getElementById('account').value !== "" ? document.getElementById('account').value : "{ACCOUNT}";
+
+    document.getElementById("password-title-preview").setAttribute("placeholder", `${company} - ${location} - ${service} - ${account}`);
+}
 
 function checkInputs(container, button, exclude = []) {
     var inputContainer = document.getElementById(container);
@@ -121,28 +136,28 @@ function checkInputs(container, button, exclude = []) {
     var allInputsNotEmpty = true;
 
     for (var i = 0; i < inputs.length; i++) {
-      if (inputs[i].value === '' && !exclude.includes(inputs[i].id)) {
-        allInputsNotEmpty = false;
-        break;
-      }
+        if (inputs[i].value === '' && !exclude.includes(inputs[i].id)) {
+            allInputsNotEmpty = false;
+            break;
+        }
     }
 
     if (allInputsNotEmpty) {
-      submitButton.removeAttribute('disabled');
+        submitButton.removeAttribute('disabled');
     } else {
-      submitButton.setAttribute('disabled', 'disabled');
+        submitButton.setAttribute('disabled', 'disabled');
     }
-  }
+}
 
 function clearPasswordInputs(alert = false) {
     document.getElementById("company-name").value = "";
-    document.getElementById("password-title-preview").innerHTML = `{CLIENT} - [ "Global" | {LOCATION} ] - {SERVICE} - {ACCOUNT}`;
-    document.getElementById('company-select').innerHTML = `<option selected disabled="disabled">Select Company</option>`;
-    document.getElementById('location').value = "" ;
-    document.getElementById('service').value = "" ;
-    document.getElementById('account').value = "" ;
-    document.getElementById('username').value = "" ;
-    document.getElementById('password').value = "" ;
+    document.getElementById("password-title-preview").setAttribute("placeholder", `{CLIENT} - [ "Global" | {LOCATION} ] - {SERVICE} - {ACCOUNT}`);
+    document.getElementById('company-select').innerHTML = `<option style="display: none">Select Company</option>`;
+    document.getElementById('location').value = "";
+    document.getElementById('service').value = "";
+    document.getElementById('account').value = "";
+    document.getElementById('username').value = "";
+    document.getElementById('password').value = "";
     document.getElementById("password-title-preview").setAttribute('company-id', "");
     document.getElementById('psw-naming-convention-apply').setAttribute('disabled', 'disabled')
 
